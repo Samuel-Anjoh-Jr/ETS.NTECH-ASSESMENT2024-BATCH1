b22a9334a1ad6e47a9b49b5ad9fefaa66ade9752
Samuel-Anjoh-Jr <anjohsamueljr@gmail.com>
2024-08-17 15:30:43 +0100

    Initial commit

---
+++ ntech/js/app.js
@@ -0,0 +1,9 @@
+async function includeHTML(elementId, file) {
+    const response = await fetch(file);
+    const htmlContent = await response.text();
+    document.getElementById(elementId).innerHTML = htmlContent;
+  }
+  
+  includeHTML('samuel','/pages/samuel_page.html');
+  includeHTML('pride','/pages/pride_page.html');
+  includeHTML('prince','/pages/prince_page.html');
